5a1339204e62fe78d0c8dcc04c82550eabee1298
JessicaBoissard2019 <163436949+JessicaBoissard2019@users.noreply.github.com>
2024-06-24 10:59:14 -0400

    initial commit

---
+++ TestDogs.java
@@ -0,0 +1,18 @@
+// Chapter4 Exercise (pages 141-143)
+
+public class TestDogs {
+    public static void main(String[] args) {
+        DogTri dog1 = new DogTri("Bowser", 2, 85, 89, 0);
+        dog1.display();
+        System.out.println();
+
+        DogTri dog2 = new DogTri("Rush", 3, 78, 72, 80);
+        dog2.display();
+        System.out.println();
+
+        DogTri dog3 = new DogTri("Ginger", 3, 90, 86, 72);
+        dog3.display();
+        System.out.println();
+
+    }// end of main
+}// end of class block
